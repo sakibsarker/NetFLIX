72b6d91f193205ce8fdedfa8f253a1b207e922c8
sakibsarker <sakib15-3824@diu.edu.bd>
2023-05-14 20:18:31 +0600

    login and profile info show

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,6 +1,21 @@
 import { NextApiRequest,NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import pirsmadb from "@/lib/prismadb"
+import prismadb from "@/lib/prismadb"
 const serverAuth=async(req:NextApiRequest)=>{
-    const session=await getSession({req})
+    const session=await getSession({req});
+    if(!session?.user?.email){
+        throw new Error('Not signed in');
+    }
+    const currentUser=await prismadb.user.findUnique({
+        where:{
+            email:session.user.email,
+        }
+    });
+
+    if(!currentUser){
+        throw new Error('Not signed in');
+    }
+    return {currentUser};
 }
+
+export default serverAuth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
+import userCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession,signOut } from 'next-auth/react';
 import Image from 'next/image'
-import React, { useCallback, useState } from 'react';
-export default function Home() {
-  const [count, setCount] = useState(6);
 
-  // Define a callback function using useCallback
-  const handleClick = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
+export async function getServerSideProps(context:NextPageContext) {
+  const session=await getSession(context);
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent:false,
+      }
+    }
+  }
+  return{
+    props:{}
+
+  }
+}
+
+export default function Home() {
+  const {data:user}=userCurrentUser();
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={handleClick}>Increment</button>
+    <div className="flex justify-center flex-col items-center gap-8 mt-20">
+      <p className="bg-green-500 h-10 w-40 flex justify-center items-center">Netflix Dashboard</p>
+      <p className="bg-green-500 h-10 w-40 flex justify-center items-center">Logged in as : {user?.email}</p>
+      <button onClick={()=>signOut()} className="bg-green-500 rounded-md justify-center flex items-cente w-full">Logout</button>
     </div>
   )
 }
