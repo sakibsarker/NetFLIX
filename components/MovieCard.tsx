3fd1753a3659bbb9abda61e0d25cb4dd30c65368
sakibsarker <sakib15-3824@diu.edu.bd>
2023-06-21 00:07:47 +0600

    tending

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const MovieCard = () => {
+interface MovieCardProps {
+  data: Record<string, any>;
+}
+
+const MovieCard:React.FC<MovieCardProps> = ({ data }) => {
   return (
-    <div>MovieCard</div>
+    <div className="group bg-zinc-900 col-span relative h-[12vw" >
+      <img className="coursor-pointer object-cover transition duration
+      shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0
+      delay-300 w-full h-[13vw]"
+       src={data.thumbnailUrl} alt='Thumbnail'/>
+    </div>
   )
 }
 
